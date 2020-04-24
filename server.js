26266a1d1c86a14ce08db4bb6c292a1e9e85f51c
Suraj <surajchan68@gmai.com>
2020-04-24 07:50:54 +0545

    install dependencies and create server

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+const app = require('./app');
+
+
+// Creating (Initialize) Server
+const server = http.createServer(app);
+
+
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+    console.log(` Server listening on ${port} on ` + Date(new Date()));
+
+});
+
+process.on('SIGTERM', function () {
+    server.close(function () {
+        process.exit(0);
+    });
+});
+
+module.exports = server;
